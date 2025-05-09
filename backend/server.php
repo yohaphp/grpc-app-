4fd592abe42eed479f30436fe304285a88b110d4
yohacode <johnpro3269@gmail.com>
2025-05-09 11:34:17 +0300

    Backend working

--- backend/server.php
+++ backend/server.php
@@ -3,14 +3,18 @@
 require 'vendor/autoload.php';
 
 use Spiral\GRPC\Server;
-use App\Grpc\EchoService;
 use Spiral\RoadRunner\Worker;
-use Ping\PingServiceInterface;
-use App\Grpc\EchoServiceInterface;
+use Spiral\Goridge\StreamRelay;
 use Ping\PingService;
+use Ping\PingServiceInterface;
 
+// Create the relay from STDIN/STDOUT
+$relay = new StreamRelay(STDIN, STDOUT);
+
+// Pass the relay to the Worker
+$worker = new Worker($relay);
+
+// Start the GRPC server
 $server = new Server();
 $server->registerService(PingServiceInterface::class, new PingService());
-
-$worker = Worker::create();
 $server->serve($worker);
